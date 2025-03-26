19fbacad18036c2d7b38c77b3f1ccaf0681194cd
Bilash Prosad <pbilash64@gmail.com>
2025-03-26 22:51:12 +0600

    Add home navbar

---
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -0,0 +1,17 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import HomeNavbar from "../components/home-navbar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function HomeLayout({ children }: LayoutProps) {
+  return (
+    <SidebarProvider>
+      <div>
+        <HomeNavbar />
+        <div>{children}</div>
+      </div>
+    </SidebarProvider>
+  );
+}
