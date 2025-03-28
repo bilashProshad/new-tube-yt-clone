78d05ed3a097cbb2b1ddccfb3bca795e15b1014d
Bilash Prosad <pbilash64@gmail.com>
2025-03-28 22:23:40 +0600

    Set up database connection and schema

---
+++ src/db/schema.ts
@@ -0,0 +1,24 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
+    // TODO: add banner fields
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+  },
+  (t) => [uniqueIndex("clerk_id_index").on(t.clerkId)]
+);
