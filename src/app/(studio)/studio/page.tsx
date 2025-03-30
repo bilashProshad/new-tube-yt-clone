82c25dab0d6cd4e6110b5795b4a952f0c38af914
Bilash Prosad <pbilash64@gmail.com>
2025-03-31 02:04:27 +0600

    Add videos section and studio view with video creation functionality

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,3 +1,15 @@
-export default function StudioPage() {
-  return <div>StudioPage</div>;
+import { DEFAULT_LIMIT } from "@/constants";
+import StudioView from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+export default async function StudioPage() {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 }
