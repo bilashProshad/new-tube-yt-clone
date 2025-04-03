c690eebf4f2dd5f3915cf56748af3cad9967eb24
Bilash Prosad <pbilash64@gmail.com>
2025-04-04 01:47:00 +0600

    Add thumbnail upload functionality

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "image.mux.com",
       },
+      {
+        protocol: "https",
+        hostname: "6r75fs8fcu.ufs.sh",
+      },
     ],
   },
 };
