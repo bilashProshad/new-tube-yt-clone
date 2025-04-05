6d37875c14d1351a1cf1f1a693f78492c915f0af
Bilash Prosad <pbilash64@gmail.com>
2025-04-05 23:11:11 +0600

    generate video title using AI

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -8,6 +8,15 @@ interface InputType {
   videoId: string;
 }
 
+const TITLE_SYSTEM_PROMPT = `Your task is to generate an SEO-focused title for a YouTube video based on its transcript. Please follow these guidelines:
+
+- Be concise but descriptive, using relevant keywords to improve discoverability.
+- Highlight the most compelling or unique aspect of the video content.
+- Avoid jargon or overly complex language unless it directly supports searchability.
+- Use action-oriented phrasing or clear value propositions where applicable.
+- Ensure the title is 3-8 words long and no more than 100 characters.
+- ONLY return the title as plain text. Do not add quotes or any additional formatting.`;
+
 export const { POST } = serve(async (context) => {
   const input = context.requestPayload as InputType;
   const { userId, videoId } = input;
@@ -25,13 +34,46 @@ export const { POST } = serve(async (context) => {
     return existingVideo;
   });
 
-  console.log({ video });
+  const transcript = await context.run("get-transcript", async () => {
+    const trackUrl = `https://stream.mux.com/${video.muxPlaybackId}/text/${video.muxTrackId}.txt`;
+    const response = await fetch(trackUrl);
+    const text = await response.text();
 
-  await context.run("initial-step", () => {
-    console.log("initial step ran");
+    if (!text) {
+      throw new Error("Bad request");
+    }
+
+    return text;
   });
 
-  await context.run("second-step", () => {
-    console.log("second step ran");
+  const { body } = await context.api.openai.call("generate-title", {
+    token: process.env.OPENAI_API_KEY!,
+    operation: "chat.completions.create",
+    body: {
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "system",
+          content: TITLE_SYSTEM_PROMPT,
+        },
+        {
+          role: "user",
+          content: transcript,
+        },
+      ],
+    },
+  });
+
+  await context.run("update-video", async () => {
+    const title = body.choices[0]?.message.content;
+
+    if (!title) {
+      throw new Error("Bad request");
+    }
+
+    await db
+      .update(videos)
+      .set({ title: title || video.title })
+      .where(and(eq(videos.id, video.id), eq(videos.userId, video.userId)));
   });
 });
